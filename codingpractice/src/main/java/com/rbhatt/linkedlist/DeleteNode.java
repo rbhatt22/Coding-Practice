500b4558357a78ae378167754eaf0c71e664005c
rbhatt22 <rutvijbhatt22@gmail.com>
2020-06-04 17:11:44 -0400

    Leet code practice

---
+++ codingpractice/src/main/java/com/rbhatt/linkedlist/DeleteNode.java
@@ -0,0 +1,27 @@
+package com.rbhatt.linkedlist;
+
+//LinkedList
+//Leetcode question # 237
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class DeleteNode {
+
+    public void deleteNode(ListNode node) {
+
+        while (node.next.next != null) {
+            node.val = node.next.val;
+            node = node.next;
+        }
+
+        node.val = node.next.val;
+        node.next = null;
+
+    }
+
+}
